5fdca9e537e0fb61d1b5d120ded58927130ed9c4
K4r0lP <kpastuszka@student.agh.edu.pl>
2021-05-25 23:13:31 +0200

    Cwiczenie8

--- Cw8.sql
+++ Cw8.sql
@@ -1,7 +1,7 @@
 use AdventureWorks2019;
---Zad 1:
-go;
+go
 
+--Zad. 1:
 begin
 	select HumanResources.EmployeePayHistory.BusinessEntityID, FirstName, LastName, Rate, RateChangeDate
 	from HumanResources.EmployeePayHistory inner join Person.Person
@@ -9,10 +9,10 @@ begin
 	where Rate < (select avg(Rate)from HumanResources.EmployeePayHistory);
 end;
 
-go;
+go
 
---Zad 2:
-create or alter function Wysylka(@ID INT)
+--Zad. 2:
+create or alter function Wysylka(@ID int)
 returns datetime
 as
 begin
@@ -21,33 +21,42 @@ begin
 
 	return @Data
 end;
-go;
+go
 
 declare @SalesID int = 43892;
-select @SalesID, dbo.Wysylka(@SalesID);
+select @SalesID as 'ID', dbo.Wysylka(@SalesID) as 'Data wysylki';
 
-go;
+go
 
---Zad 3:
+--Zad. 3:
 
-create OR alter procedure wyswietlanie(@nazwa_pr varchar(50))
+create or alter procedure wybieranie(@name varchar(40))
 as
 begin
-	select ProductID, Name, ProductNumber, SafetyStockLevel from Production.Product where Name = @nazwa_pr;
+    declare @id int= (select ProductID from Production.Product where Name = @name);
+    declare @quantity int = (select sum(Quantity) from Production.ProductInventory where ProductID = @id);
+	IF @quantity > 0
+		select  Name, ProductID, ProductNumber, 'Dostepny' as 'Dostepnosc'
+		from Production.Product where Name = @name;
+	else
+		select  Name, ProductID, ProductNumber, 'Niedostepny' as 'Dostepnosc'
+		from Production.Product  where Name = @name;
 end;
 
-declare @nazwa varchar(50) = 'LL Crankarm';
-exec wyswietlanie @nazwa;
+go
 
-go;
+declare @name varchar(40) = 'Metal Bar 1';
+exec wybieranie @name;
 
---Zad 4:
+
+go
+--Zad. 4:
 
 create or alter function kartakredyt(@zamowienie int)
-returns varchar(20)
+returns varchar(25)
 as
 begin
-	declare @numer varchar(20);
+	declare @numer varchar(25);
 
 	select @numer = Sales.CreditCard.CardNumber 
 	from Sales.CreditCard inner join Sales.SalesOrderHeader 
@@ -60,5 +69,68 @@ go;
 declare @numer_zamowienia INT = 43892;
 select @numer_zamowienia, dbo.kartakredyt(@numer_zamowienia);
 
-go;
+go
 
+--Zad. 5
+create or alter procedure kalk(@num1 float, @num2 float)
+as 
+declare @wynik float
+declare @blad varchar(130)
+if @num1<@num2
+begin
+set @blad = 'Niewlasciwie zdefiniowane zmienne, wartosc pierwsza musi byc wieksza od drugiej';
+select @blad
+return;
+end;
+else
+begin
+set @wynik = @num1/@num2;
+select round(@wynik,2)
+return;
+end;
+
+exec kalk @num1 =44, @num2=3;
+go
+
+--Zad. 6
+create or alter procedure wolne(@id varchar(25))
+as
+begin
+	select JobTitle, VacationHours / 8 AS 'wakacyjne', SickLeaveHours / 8 AS 'chorobowe'
+	from AdventureWorks2019.HumanResources.Employee
+	where NationalIDNumber = @id; 
+end;
+
+go
+
+declare @id varchar(25);
+exec wolne @id = 276751903;
+
+
+go
+
+--Zad. 7
+create or alter procedure kalkulator_walut(@kwota float, @z varchar(10), @na varchar(10), @data_przeliczenia datetime)
+as
+begin
+	declare @kurs float;
+	declare @wynik float;
+	if @z = 'USD'
+		begin
+			select @kurs = AverageRate from Sales.CurrencyRate where ToCurrencyCode = @na and CurrencyRateDate = @data_przeliczenia;
+			set @wynik = @kurs * @kwota;
+		end;
+	else
+		begin
+			select @kurs = AverageRate from Sales.CurrencyRate where ToCurrencyCode = @z and CurrencyRateDate = @data_przeliczenia;
+			set @wynik = @kwota/@kurs;
+		end;
+	select @z as z, @na as na, @kwota as kwota, @kurs as kurs, @wynik as wynik;
+end;
+
+go
+declare @waluta1 varchar(5) = 'GBP';
+declare @waluta2 varchar(5) = 'USD';
+declare @kwota float = 2350;
+declare @data_przeliczenia datetime = '2013-05-31 00:00:00.000'
+exec kalkulator_walut @kwota, @waluta1, @waluta2, @data_przeliczenia;
